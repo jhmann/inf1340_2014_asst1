8ec2d1c5a55761e48f3bbddf6bfdba3383cafb8e
jhmann <jessmann74@gmail.com>
2014-10-01 17:43:24 -0400

    Version with code and comments

--- exercise3.py
+++ exercise3.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python3
 
-comparator = {}
-comparator[("Scissors", "Scissors")] = 0
-comparator[("Scissors", "Paper")] = 1
-comparator[("Scissors", "Rock")] = 2
-comparator[("Paper", "Scissors")] = 2
-comparator[("Paper", "Paper")] = 0
-comparator[("Paper", "Rock")] = 1
-comparator[("Rock", "Scissors")] = 1
-comparator[("Rock", "Paper")] = 2
-comparator[("Rock", "Rock")] = 0
+"""
+    Return the result of a rock, paper, scissors game
+
+    Assignment 1, Exercise 3, INF1340 Fall 2014
+"""
+
+__author__ = "Jessica Mann and Juntian Wang"
+__email__ = "jess.mann@mail.utoronto.ca and justinjtwang@gmail.com"
+
+__copyright__ = "2014 Jessica Mann and Juntian Wang"
+__license__ = "MIT License"
+
+__status__ = "Version 2"
 
 
+# create the variable "game_rules" as an empty dictionary to hold the rules for the game
+# created & populated outside the function for efficiency: create and populate once, not every time the function runs
+game_rules = {}
+
+# add elements to the data dictionary game_rules
+# each line adds a key-value pair
+# each key is player1's parameter, player 2's parameter
+# each value is 0 for tie, 1 if player1 wins, 2 if player2 wins
+game_rules[("Scissors", "Scissors")] = 0
+game_rules[("Scissors", "Paper")] = 1
+game_rules[("Scissors", "Rock")] = 2
+game_rules[("Paper", "Scissors")] = 2
+game_rules[("Paper", "Paper")] = 0
+game_rules[("Paper", "Rock")] = 1
+game_rules[("Rock", "Scissors")] = 1
+game_rules[("Rock", "Paper")] = 2
+game_rules[("Rock", "Rock")] = 0
+
+
+#create a function called decide_rps
 def decide_rps(player1, player2):
-    return comparator[(player1, player2)]
-
-
+    """(string, string) -> int
+    Return the key for value inputs player1 and player2 as contained in game_rules
+    Assumes only valid inputs of Scissors, Paper or Rock
+    >>> game_rules[("Scissors", "Scissors")]
+    0
+    """
+    return game_rules[(player1, player2)]
